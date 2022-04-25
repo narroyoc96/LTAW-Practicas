977b8e075df11b3400c0154a04485e00921e83ee
narroyoc <n.arroyoc.2017@alumnos.urjc.es>
2022-04-25 21:42:18 +0200

    puerto okkk

--- P3/chat-server.js
+++ P3/chat-server.js
@@ -8,7 +8,7 @@ const http = require('http');
 const express = require('express');
 const colors = require('colors');
 
-const PUERTO = 2424;
+const PUERTO = 9090;
 
 let welcome_message = ">> ¡Hola, bienvenido al chat!";
 let new_user = ">> Un nuevo usuario ha entrado en el chat";
